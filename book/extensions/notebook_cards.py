1f8d832bfa23cd631d152b629a96519529bc683d
Christoph Reimer <christoph.reimer@eodc.eu>
2024-12-12 17:29:59 +0100

    initial version of gallery structure.

---
+++ book/extensions/notebook_cards.py
@@ -0,0 +1,67 @@
+from glob import glob
+from pathlib import Path
+import yaml
+import textwrap
+
+def clean_f_string(string):
+    return '\n'.join([m.lstrip() for m in string.split('\n')])
+
+def main(app,config):
+  # find all ipynb files in current project
+  ipynb_list = glob(f"./**/*.ipynb", recursive=True)
+  domains = []
+  keys = []
+  for nb in ipynb_list:
+    path_parents = nb.split("/")
+    domains.append(path_parents[1])
+    if len(path_parents) > 3:
+      keys.append(path_parents[2])
+  domains = set(domains)
+  domains.remove("book")
+  keys = set(keys)
+
+  for domain in domains:
+    # domain gallery template
+    gallery = textwrap.dedent(
+      """
+      # {domain} Gallery
+
+      ::::{{grid}} 2
+      :gutter: 2
+      {grid_cards}
+      ::::
+    """)
+
+    flist_domain = glob(f"./{domain}/**/*.ipynb", recursive=True)
+    grid_cards = []
+    for nb in flist_domain:
+      new_card = f"""
+          :::{{grid-item-card}} title
+          :shadow: md
+          :link: https://example.com
+          :img-top: 
+          description
+          +++
+          footer
+          :::
+      """
+      grid_cards.append(clean_f_string(new_card))
+    grid_cards = "\n".join(grid_cards)
+
+    Path(f"./{domain}/gallery.md").write_text(
+      gallery.format(
+        domain=domain,
+        grid_cards=grid_cards
+      ))
+  
+  pass
+
+def setup(app):
+    #app.connect("builder-inited", main)
+    app.connect("config-inited", main)
+    return {
+      'version': '0.1',
+      'env_version': 1,
+      'parallel_read_safe': True,
+      'parallel_write_safe': True,
+    }
---
+++ generate_toc.py
@@ -0,0 +1,69 @@
+from glob import glob
+from pathlib import Path
+import textwrap
+
+def clean_string(string):
+    return '\n'.join([m.lstrip() for m in string.split('\n')])
+
+def run_toc_generation():
+  # find all ipynb files in current project
+  ipynb_list = glob(f"./**/*.ipynb", recursive=True)
+  domains = []
+  keys = []
+  for nb in ipynb_list:
+    path_parents = nb.split("/")
+    domains.append(path_parents[1])
+    if len(path_parents) > 3:
+      keys.append(path_parents[2])
+  domains = set(domains)
+  domains.remove("book")
+  keys = set(keys)
+
+  # create chapter per domain
+  toc = []
+  for domain in domains:
+    flist_domain = glob(f"./{domain}/**/*.ipynb", recursive=True)
+    domain_ipynb = ""
+    for nb in flist_domain:
+      fname = Path(nb).with_suffix('')
+      domain_ipynb += f"- file: {fname}\n"
+    domain_ipynb = textwrap.indent(domain_ipynb, "      ", lambda line: True)
+
+    chapter = textwrap.dedent("""
+    - caption: {domain_name}
+      chapters:
+        - file: {domain_name}/README
+          title: Overview
+        - file: {domain_name}/gallery
+          title: {domain_name} - Gallery
+          sections:
+    {files}
+    """)
+
+    toc.append(
+      chapter.format(
+        domain_name=domain,
+        files=domain_ipynb)
+    )
+  
+  # join toc items to single string
+  toc_items= "".join(toc)
+  #print(toc_items)
+
+  # _toc.yml template to be written
+  toc_template = textwrap.dedent(
+    """
+    format: jb-book
+    root: {root_doc}
+    parts:
+    {toc_entries:>4}
+    """
+  )
+  return toc_template.format(root_doc="README", toc_entries=toc_items)
+
+if __name__ == '__main__':
+  toc_file = f"./book/_toc.yml"
+  toc = run_toc_generation()
+  Path(toc_file).write_text(toc)
+
+  
