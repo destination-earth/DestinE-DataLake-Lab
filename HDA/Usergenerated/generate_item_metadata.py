b2276315c39429fcfdcc9cd8dad50f041282087d
katharina.starzer@eodc.eu <katharina.starzer@eodc.eu>
2025-11-04 14:38:41 +0100

    merge main into staging

--- HDA/Usergenerated/generate_item_metadata.py
+++ HDA/Usergenerated/generate_item_metadata.py
@@ -82,7 +82,7 @@ class ItemGenerator:
 
         ##### Load and Validate the Collection #####
         collection = confighelper.load_and_validate_collection(
-            self.collection_path, self.collection_id
+            self.collection_path, self.collection_id, save_reordered_collection=False, is_compare_expected_id=True
         )
 
         ###### load the collection config #####
@@ -147,6 +147,9 @@ class ItemGenerator:
         # Iterate over Item folders to create Item files
         for item_folder_path in item_folder_paths:
             self.create_item(item_folder_path, collection, collection_config)
+
+        # Assure that item assets are always ordered consistently
+        confighelper.sort_item_assets_in_folder(self.items_root)
 
         if IS_UPLOAD_S3:
 
@@ -323,11 +326,11 @@ class ItemGenerator:
 
             # Set collection and item self links
             collection.set_self_href(
-                f"https://hda.data.destination-earth.eu/stac/collections/{collection_id}"
+                f"https://hda.data.destination-earth.eu/stac/v2/collections/{collection_id}"
             )
             item.set_collection(collection)
             item.set_self_href(
-                f"https://hda.data.destination-earth.eu/stac/collections/{collection_id}/items/{item_id}"
+                f"https://hda.data.destination-earth.eu/stac/v2/collections/{collection_id}/items/{item_id}"
             )
 
             # Ensure the items_root directory exists
